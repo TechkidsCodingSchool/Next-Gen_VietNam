fca464602248a3488ba0504c6144b1a2a72a0863
MinhNC <45997670+MinhNCnextgen@users.noreply.github.com>
2019-01-12 05:03:42 -0800

    Update projectnextgenjavascript.js

--- project/projectnextgenjavascript.js
+++ project/projectnextgenjavascript.js
@@ -1,3 +1,7 @@
+/* form:
+	{titles:"",content:"",img:"imgs/",date:""},
+
+*/
 posts=[
 	{titles:"Recommended places to travel",content:"Find out about the most favourites places travellers go across Vietnam,from north to south.",img:"imgs/vietnam.png",link:"projectnextgen-post1.html",date:"14/3/2018"},
 	{titles:"Things to be bought with in Vietnam",content:"You can't travel to any country without any items in your backpack!In vietnam,these are the top things you need to bring.",img:"imgs/hanoi.png",link:"projectnextgen-post2.html",date:"16/7/2018"},
@@ -5,7 +9,7 @@ posts=[
 ]
 			createPosts();
 			function createPosts(){
-				for (var i=0;i<3;i++){
+				for (var i=0;i<posts.length;i++){
 					var btn=document.createElement("DIV");
 					document.getElementById("section").appendChild(btn); 
 					btn.id = "post" + i; 
@@ -17,8 +21,7 @@ posts=[
 					document.getElementById(btn.id).appendChild(title); 
 					document.getElementById(btn.id).appendChild(content); 
 					document.getElementById(btn.id).appendChild(date); 
-					document.getElementById(btn.id).appendChild(document.createElement("hr")); 
-					
+					document.getElementById(btn.id).appendChild(document.createElement("hr")); 				
 				}
 				
 			}
